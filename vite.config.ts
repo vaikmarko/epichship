d9c824a0ab26bfc5fe5acad8d6f18ff4e74182a8
Marko <vaikmarko@gmail.com>
2026-01-02 18:38:33 +0200

    Fix: Rename assetsDir to resources to avoid caching issues and restore absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Relatiivsed teed on turvalisemad
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'resources', // Muudame kausta nime, et vältida konflikte
+  }
 })
